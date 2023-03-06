be5436046643211cc88480fd5d9b42b3fce46d27
mnaibei <muchajulius@gmail.com>
2023-03-06 22:36:26 +0300

    Fixing hamburger menu

--- mobile.js
+++ mobile.js
@@ -1,14 +1,12 @@
-const ambuger = document.querySelector(".ambuger");
-const mobileMenu = document.querySelector(".mobile-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-ambuger.addEventListener("click", () => {
-    ambuger.classList.toggle("active");
-    mobileMenu.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
-document.querySelectorAll(".mobile-list").forEach(n => n. addEventListener("click", () =>{
-    ambuger.classList.remove("active");
-    mobileMenu.classList.remove("active");
-
-    
-}))
+document.querySelectorAll('.nav-menu').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
