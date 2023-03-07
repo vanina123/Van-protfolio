4392e0f9291e2e2a547e4aa5004e5b14f362893b
mnaibei <muchajulius@gmail.com>
2023-03-07 09:57:28 +0300

    Fixed mobile menu issue

--- mobile.js
+++ mobile.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
+console.log(hamburger)
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
