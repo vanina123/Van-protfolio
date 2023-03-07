5a2437b04b08dd3062477ce110f11085e3cf41db
nana vanina <vaninalarissa@gmail.com>
2023-03-07 08:57:44 +0100

    modify css file

--- mobile.js
+++ mobile.js
@@ -1,8 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
-console.log(hamburger)
-
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
